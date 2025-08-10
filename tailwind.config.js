5d06d1b4a383bf1c2c8171fd3ec4ce029dd9bd81
Rainer Carneiro <rainercarneiro@outlook.com>
2025-08-10 11:48:46 -0300

    Estrutura inicial do formulario

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./*.{html, js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
