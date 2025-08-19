3462298f42e90568c67a37158bc047e4d5700f19
Rainer Carneiro <rainercarneiro@outlook.com>
2025-08-18 21:03:45 -0300

    Lógica principal do formulário concluída

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { generateReturnsArray } from "./calculateFee.js"
 
 //Coletando as variáveis do formulário
 let form = document.getElementById('mainForm')
+let buttonClean = document.getElementById('buttonClean')
 
 function renderProgression(event) {
     event.preventDefault()
@@ -22,14 +23,6 @@ function renderProgression(event) {
     console.log(returnsArray)
 }
 
-// function validateInitialAmount(){
-//     const initialAmountValue = Number(document.getElementById('initialAmount').value)
-//     const initialAmountDiv = Number(document.getElementById('initialAmountDiv').value)
-//     if(initialAmountValue<=0){
-//         initialAmountDiv.classList.add('error')
-//     }
-// }
-
 function validateInputs(event){
     let target = event.target
     let valueTarget = event.target.value.replace(',', '.')
@@ -41,11 +34,11 @@ function validateInputs(event){
     }
     
     if(isNaN(valueTarget) || Number(valueTarget) <=0){
-        let queryError = document.querySelector('.error')
+        let queryError = targetParent.querySelector('.error')
         if(!queryError){
             let errorMessage = document.createElement('p')
             errorMessage.textContent = 'Este campo precisa ser um número maior que 0'
-            errorMessage.classList.add('text-red-500')
+            errorMessage.classList.add('errorMessage')
 
             targetParent.classList.add('error')
             targetGrandParent.appendChild(errorMessage)
@@ -53,11 +46,8 @@ function validateInputs(event){
     }
 
     else{
-        let queryError = document.querySelector('.error')
-        if(queryError){
-            queryError.remove()
-            targetParent.classList.remove('error')
-        }
+        targetParent.classList.remove('error')
+        targetGrandParent.querySelector('.errorMessage').remove()
     }
     
 }
@@ -70,5 +60,22 @@ function applyErrorMessage(){
     }
 }
 
-applyErrorMessage()
-form.addEventListener('submit', renderProgression)
+function cleanInputs(){
+    for(let formElement of form){
+        if(formElement.tagName === 'INPUT' && formElement.hasAttribute('name')){
+            formElement.value = ''
+            formElement.parentElement.classList.remove('error')
+            const errorMessage = formElement.parentElement.parentElement.querySelector('.errorMessage')
+            if(errorMessage){
+                errorMessage.remove()
+            }
+        }
+        
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  applyErrorMessage();
+  form.addEventListener('submit', renderProgression);
+  buttonClean.addEventListener('click', cleanInputs);
+});
